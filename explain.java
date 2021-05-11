f6baf9de34aeb17b6c56bf3ee2fbe034930b317b
kevink2k19 <80607838+kevink2k19@users.noreply.github.com>
2021-05-12 00:09:32 +0630

    Open Form of  Grade Test

---
+++ explain.java
@@ -0,0 +1,12 @@
+package swag;
+
+public class changesMark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double numb = 50/10.0; //60 go to 60.0 ,70 to 70.00
+		int mark = (int) (numb + 0.0); //round float format , 70.0 goes 70
+		System.out.println(mark);
+	}
+
+}
